f2fbdbff3e2b970bd6406d34e043d8704db62959
Michał Zdunek <mzdunek93@gmail.com>
2019-08-11 18:39:31 +0200

    Task 5 - comments

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,11 +1,12 @@
 class MoviesController < ApplicationController
-  before_action :authenticate_user!, only: [:send_info]
+  before_action :authenticate_user!, only: [:send_info, :add_comment]
 
   def index
     @movies = Movie.all.decorate
   end
 
   def show
+    @comment = Comment.new
     @movie = Movie.find(params[:id])
   end
 
@@ -19,5 +20,26 @@ class MoviesController < ApplicationController
     file_path = "tmp/movies.csv"
     MovieExporter.new.call(current_user, file_path)
     redirect_to root_path, notice: "Movies exported"
+  end
+
+  def add_comment
+    movie = Movie.find(params[:id])
+    if movie.comments.any? { |comment| comment.user_id == current_user.id }
+      redirect_back(fallback_location: root_path, alert: "You already posted a comment")
+    else
+      comment = Comment.new(comment_params)
+      comment.movie = movie
+      comment.user = current_user
+      if comment.save
+        redirect_back(fallback_location: root_path, notice: "Comment added")
+      else
+        redirect_back(fallback_location: root_path, alert: "Error when adding comment")
+      end
+    end
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:content)
   end
 end
