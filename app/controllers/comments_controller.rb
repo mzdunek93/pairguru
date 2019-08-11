cdfbe6ddd356377da718cc36bcf5c1831642dd7f
Michał Zdunek <mzdunek93@gmail.com>
2019-08-11 18:57:00 +0200

    sql query

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -11,4 +11,15 @@ class CommentsController < ApplicationController
       redirect_back(fallback_location: root_path, alert: "Error when deleting comment")
     end
   end
+
+  def top_commenters
+    @commenters = Comment
+      .select("COUNT(comments.user_id) as comments_count, users.*")
+      .joins(:user)
+      .where("comments.created_at >= :date", date: 7.days.ago)
+      .group(:user_id)
+      .order("comments_count DESC")
+      .limit(10)
+    puts @commenters.to_json
+  end
 end
