f126b0782bc9cbef286669fd58739128d5e9ad30
Michał Zdunek <mzdunek93@gmail.com>
2019-08-11 19:37:23 +0200

    test fix

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -3,5 +3,6 @@ FactoryBot.define do
     name { Faker::Internet.username }
     email { Faker::Internet.email }
     password { Faker::Internet.password(min_length: 8) }
+    confirmed_at { Time.zone.now }
   end
 end
--- spec/jobs/send_movie_info_job_spec.rb
+++ spec/jobs/send_movie_info_job_spec.rb
@@ -4,7 +4,9 @@ RSpec.describe SendMovieInfoJob, type: :job do
   let!(:movies) { create_list(:movie, 5, title: 'Godfather') }
   it "sends movie info" do
     ActiveJob::Base.queue_adapter = :test
+    user = create(:user)
+    sign_in user
     visit "/movies/1/send_info"
-    expect(SendMovieInfoJob).to have_been_enqueued.exactly(:once).with(nil, Movie.find(1))
+    expect(SendMovieInfoJob).to have_been_enqueued.exactly(:once).with(user, Movie.find(1))
   end
 end
